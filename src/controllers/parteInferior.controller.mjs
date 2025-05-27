5430de5bf7badfe974957e0347b966b1cba758d7
Esteban Osuna <estebanosunaing@gmail.com>
2025-05-26 19:55:31 -0500

    [+] router : vincula el get /api/parteInferior/:id  , controller : consulta para buscar el id en especifico

--- src/controllers/parteInferior.controller.mjs
+++ src/controllers/parteInferior.controller.mjs
@@ -2,13 +2,9 @@ import parteInferiorModel from "../schemas/parteInferior.schema.mjs";
 
 const crearParteInferior = async (req, res) => {
     const inputData = req.body;
-
-
-    
     // CONTROLA LAS EXCEPCIONES DE LA CONSULTA A LA BASE DE DATOS
 
     try{
-
     const registeredparteInferior = await parteInferiorModel.create( inputData); 
 
     console.log(registeredparteInferior);  //imprime en la consola
@@ -26,19 +22,47 @@ const crearParteInferior = async (req, res) => {
 const getAllParteInferior = async (req, res) => {
     
     try{
-
-    
     const data = await parteInferiorModel.find({});
     res.json( data );
     }
+
     catch(error){
         console.error(error);
         res.json({msg: 'Error: No se pudo obtener el listado de producto'})
     }
 }
 
+const getParteInferiorById = async (req, res) => {
+    const parteInferiorId = req.params.id; //El nombre final dependera del nombre del parametro en la ruta 
+
+    try{
+    
+        const data = await parteInferiorModel.findById(parteInferiorId);
+        //  const data = await parteInferiorModel.findOne({_id:parteInferiorId
+        //  });
+        
+        if(data == null){
+
+            return res.json({msg: 'Error: El producto no se encuentra registrado'});
+        }
+
+        res.json(data); 
+
+    }
+
+
+    catch (error){
+        console.error( error);
+        res.json({msg: 'Error: No se pudo encontrar el producto'});
+    }
+}
+
+
+
+
 export {
     crearParteInferior,
-    getAllParteInferior
+    getAllParteInferior,
+    getParteInferiorById
 }
 
--- src/routes/miArmario.route.mjs
+++ src/routes/miArmario.route.mjs
@@ -1,14 +1,17 @@
 import express from 'express';
 import {  crearParteSuperior } from '../controllers/parteSuperior.controller.mjs';
-import { crearParteInferior, getAllParteInferior } from '../controllers/parteInferior.controller.mjs';
+import { crearParteInferior, getAllParteInferior, getParteInferiorById } from '../controllers/parteInferior.controller.mjs';
 
 const router = express.Router();
 
 //Definir las rutsa para la entidad miArmario
 // router.post('/api/miArmario',crearMiArmario);
 router.post('/api/parteSuperior',crearParteSuperior );
-router.get('/api/parteInferior', getAllParteInferior)
+
+
 router.post('/api/parteInferior',crearParteInferior);
+router.get('/api/parteInferior', getAllParteInferior);
+router.get('/api/parteInferior/:id', getParteInferiorById);  //:id = Parametrizar la ruta: Creamos una especie de variable
 
 
 export default router;
